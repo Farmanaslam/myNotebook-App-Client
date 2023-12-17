928e5a0625c46755c3bdf7b2dddc50a23057731b
farman <farmanaslam674@gmail.com>
2023-12-17 17:37:17 +0530

    deleted backend

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose");
-const mongoURI="mongodb://0.0.0.0:27017/mynotebook";
-
-const connectToMongo=()=>{
-    mongoose.connect(mongoURI);
-    console.log("Mongoose Connected");
-}
-module.exports=connectToMongo;
