1aa6e4e8f57c41d7d1b959782cf6f2f42aeb5fff
@Farman <112494635+Farmanaslam@users.noreply.github.com>
2023-12-18 20:23:34 +0530

    Update Login.js

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -8,7 +8,7 @@ const Login = (props) => {
   let navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://mern-api-ruby.vercel.app/api/auth/login", {
+    const response = await fetch("https://cors-anywhere.herokuapp.com/https://mern-api-ruby.vercel.app/api/auth/login", {
     // const response = await fetch("http://localhost:5000/api/auth/login", {
       method: "POST",
 
